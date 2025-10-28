4d2888f97660cb6dfd1ce912c1056f21efc47dff
Avneet-22 <kaura22@farmingdale.edu>
2025-10-28 14:07:56 -0400

    Ideas

--- WorkoutPlanning.java
+++ WorkoutPlanning.java
@@ -1,5 +1,14 @@
 public class WorkoutPlanning {
     public static void main(String[] args){
-        System.out.println();
+        System.out.println("ooo fall");
     }
+
+// parent class method
+    // if else statements
+    //data classes: age, gender, ints
+    // double weight,
+    // goal,name, cal goal string
+    // what's your main focus
+
+
 }
