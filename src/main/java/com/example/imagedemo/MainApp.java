a0133e7a2a7cf007868e6a663b94b66eb62ebef7
ShivsterNk <siva.nirmalakumar@gmail.com>
2025-11-04 14:21:59 -0500

    Created Gym Map image load

---
+++ src/main/java/com/example/imagedemo/MainApp.java
@@ -0,0 +1,25 @@
+package com.example.imagedemo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Image Display");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.imagedemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.imagedemo to javafx.fxml;
+    exports com.example.imagedemo;
+}
