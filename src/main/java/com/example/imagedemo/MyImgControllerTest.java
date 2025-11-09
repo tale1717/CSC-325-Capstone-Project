30940e05b4ee5035e27ca6c2e6f3dcbab2efaae9
ShivsterNk <siva.nirmalakumar@gmail.com>
2025-11-08 22:21:13 -0500

    Revert "Added a Gym Layout Controller File to handle the user pressing the return to home page after they view the Gym Layout Map."
    This reverts commit 637acf992f3e4f4f1518d39233de9d9fc049e207.

--- src/main/java/com/example/imagedemo/MyImgControllerTest.java
+++ src/main/java/com/example/imagedemo/MyImgControllerTest.java
@@ -2,14 +2,9 @@ package com.example.imagedemo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 
 import java.io.InputStream;
 
@@ -62,5 +57,4 @@ public class MyImgControllerTest {
         labelTreadmill.setVisible(false);
         labelTreadmill.setManaged(false);
     }
-
 }
