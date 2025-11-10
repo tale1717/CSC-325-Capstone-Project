6306c127b3be7ee3016caf09e7c2f1cac99ec96d
David T <dstalero71@gmail.com>
2025-11-09 20:48:41 -0500

    Test if key.json doesnt push

--- src/main/java/com/example/firebaseadminjavafx/Main.java
+++ src/main/java/com/example/firebaseadminjavafx/Main.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//tomas ur the goat
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
