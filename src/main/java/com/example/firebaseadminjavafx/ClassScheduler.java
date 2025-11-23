302fd3bb6330f515f31fa9df11dcd268376fe483
ShivsterNk <siva.nirmalakumar@gmail.com>
2025-11-22 22:09:06 -0500

    Created Class Scheduler and Admin Class Controllers with FXML Files.

--- src/main/java/com/example/firebaseadminjavafx/ClassScheduler.java
+++ src/main/java/com/example/firebaseadminjavafx/ClassScheduler.java
@@ -1,4 +1,32 @@
 package com.example.firebaseadminjavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ClassScheduler {
+    @FXML
+    private Button suggestClassButton;
+
+    @FXML
+    private Button searchClassButton;
+
+    @FXML
+    private Button backButton;
+
+    //PLACEHOLDER FXML FILE
+    @FXML
+    private void handleSuggestClass(){
+        Main.setRoot("class-scheduler.fxml", suggestClassButton);
+    }
+
+    //PLACEHOLDER FXML FILE
+    @FXML
+    private void handleSearchClass() {
+        Main.setRoot("admin-class-view.fxml", searchClassButton);
+    }
+
+    @FXML
+    private void handleBackToMain() {
+        Main.setRoot("gymapp-home.fxml", backButton);
+    }
 }
