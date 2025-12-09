56c52214d2c9e164126f5f88bc3962af36a4d2ef
tale1717 <dstalero71@gmail.com>
2025-12-09 14:34:52 -0500

    Update

--- src/main/java/com/example/firebaseadminjavafx/FirebaseService.java
+++ src/main/java/com/example/firebaseadminjavafx/FirebaseService.java
@@ -14,7 +14,7 @@ public final class FirebaseService {
         if (initialized) return;
         try {
 
-            FileInputStream serviceAccount = new FileInputStream("com/example/firebaseadminjavafx/key.json");
+            FileInputStream serviceAccount = new FileInputStream("src/main/key.json");
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
